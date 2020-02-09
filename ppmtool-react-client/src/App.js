29049b615824214eaa1070941b85a7750b623ad3
Aidas Pupelis <aidas.pupelis@gmail.com>
2020-02-09 11:50:45 +0200

    Handle Login logic

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
@@ -14,8 +14,29 @@ import UpdateProjectTask from "./components/ProjectBoard/ProjectTasks/UpdateProj
 import Landing from "./components/Layout/Landing";
 import Register from "./components/UserManagement/Register";
 import Login from "./components/UserManagement/Login"; 
+import jwt_decode from "jwt-decode";
+import setJWTToken from "./securityUtils/setJWTToken";
+import { SET_CURRENT_USER } from "./actions/types";
 
-function App() {
+const jwtToken = localStorage.jwtToken;
+
+if (jwtToken) {
+  setJWTToken(jwtToken);
+  const decoded_jwtToken = jwt_decode(jwtToken);
+  store.dispatch({
+    type: SET_CURRENT_USER,
+    payload: decoded_jwtToken
+  });
+
+  const currentTime = Date.now() / 1000;
+  if (decoded_jwtToken.exp < currentTime) {
+    //handle logout
+    //window.location.href = "/";
+  }
+}
+
+class App extends Component {
+  render() {
   return (
     <Provider store={store}>
       <Router>
@@ -50,6 +71,7 @@ function App() {
       </Router>
     </Provider>
   );
+}
 }
 
 export default App;
