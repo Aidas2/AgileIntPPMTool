360e38b6bccbd17e50e182e86bc48b3ab6bd837e
Aidas Pupelis <aidas.pupelis@gmail.com>
2020-02-10 20:39:23 +0200

    Bug Fixes

--- ppmtool-react-client/src/store.js
+++ ppmtool-react-client/src/store.js
@@ -8,24 +8,25 @@ const middleware = [thunk];
 
 let store;
 
-if (window.navigator.userAgent.includes("Chrome")) {
+const ReactReduxDevTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+if (window.navigator.userAgent.includes("Chrome") && ReactReduxDevTools) {
   store = createStore(
     rootReducer,
     initalState,
     compose(
       applyMiddleware(...middleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
+      ReactReduxDevTools
     )
   );
-} else if (window.navigator.userAgent.includes("Firefox")) {
+} else if (window.navigator.userAgent.includes("Firefox") && ReactReduxDevTools) {
     store = createStore(
       rootReducer,
       initalState,
       compose(
         applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-          window.__REDUX_DEVTOOLS_EXTENSION__()
+        ReactReduxDevTools
       )
     );
   } else {
